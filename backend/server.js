66a269f01aac09bbf8d37e19bbe051619178b793
balan081194 <123051259+balan081194@users.noreply.github.com>
2023-10-26 13:09:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const db = require('./db');
 const Pizza = require('./models/pizzaModel');
 const app = express();
+const cors = require("cors")
 const pizzasRoute = require('./routes/pizzaRoutes');
 const userRoute = require('./routes/userRoutes');
 
 app.use(express.json());
+app.use(cors())
 
 app.use('https://pizza-app-frontend-sooty.vercel.app/api/pizzas', pizzasRoute);
 app.use('https://pizza-app-frontend-sooty.vercel.app/api/users', userRoute);
